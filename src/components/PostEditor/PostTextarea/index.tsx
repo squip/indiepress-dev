049ab65118a7f44ef4575d2b2063175140b95fc5
Squip <skip@skip.sx>
2025-12-14 20:43:49 -0800

    ring shadow effect fixed on New Post flow

--- src/components/PostEditor/PostTextarea/index.tsx
+++ src/components/PostEditor/PostTextarea/index.tsx
@@ -92,7 +92,7 @@ const PostTextarea = forwardRef<
       editorProps: {
         attributes: {
           class: cn(
-            'border rounded-lg p-3 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background focus-visible:border-transparent',
+            'border rounded-lg p-3 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background focus-visible:ring-inset focus-visible:ring-offset-0 focus-visible:border-transparent',
             className
           )
         },
